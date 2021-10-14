1fed9b78c3eb9862f3fc880075d302c3b996de55
Hugo Barral <hugo.barral@outlook.fr>
2021-10-14 12:11:03 +0200

    Commencement du driver ADC

--- Drivers/Source/Driver_ADC.c
+++ Drivers/Source/Driver_ADC.c
@@ -7,6 +7,35 @@ void ADC_Init (ADC_TypeDef * ADC) {
 		RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
 }
 
-void ADC_Add (ADC_TypeDef * ADC, char Channel, char Group) {
+void ADC_Add_Regular(ADC_TypeDef * ADC, char Channel) {
+	// Vérification du nombre de conversions
+	char nb = ((ADC->SQR1 >> 20) & 0xF);
+	if (nb < 15) {
+		// Si l'on n'est à moins de 16 conversions
+		// on verifie que notre première conversion ne soit pas vide
+		if ((ADC->SQR3 & 0x1F) == 0) {
+			ADC->SQR1 += (1 << 20);
+			nb += 1;
+		}
+	}
 	
+	// Selon le nombre de conversions, on décale les registres
+	if (nb > 6) {
+		if (nb > 12) {
+			int sqr1 = ((ADC->SQR1 << 5) & 0x000FFFFF);
+			ADC->SQR1 &= 0xFFF00000;
+		}
+	}
+}
+
+void ADC_Add_Injected(ADC_TypeDef * ADC, char Channel) {
+	
+}
+
+void ADC_Add (ADC_TypeDef * ADC, char Channel, char Group) {
+	if (Group) {
+		ADC_Add_Regular(ADC, Channel);
+	} else {
+		ADC_Add_Injected(ADC, Channel);
+	}
 }
