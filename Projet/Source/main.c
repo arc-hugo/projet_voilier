7e9c329b889d718d38bd8b1fe9a31ad69af47f1c
Clement-INS <63722692+Clement-INS@users.noreply.github.com>
2021-11-12 14:15:05 +0100

    ADC

--- Projet/Source/main.c
+++ Projet/Source/main.c
@@ -2,11 +2,18 @@
 #include "Orientation.h"
 #include "Communication.h"
 #include "Accelero.h"
+#include "Driver_ADC.h"
+#include "Driver_GPIO.h"
 
+int res;
 int main(void){
+	res = 0;
 	/*Voile_Init();
 	Orientation_Init();
 	Communication_Init();*/
-	Run();
-	while(1);
+	GPIO_Init(GPIOB, 0, In_Analog);
+	ADC_Init(ADC1, 8);
+	while(1){
+		res = Start_Conv(ADC1);
+	};
 }	
