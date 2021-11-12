7f2561b85f0fcdf1bfc63be709980ddd6dc2b2cc
Clement-INS <63722692+Clement-INS@users.noreply.github.com>
2021-11-12 13:26:41 +0100

    tentative SPI

--- Projet/Source/main.c
+++ Projet/Source/main.c
@@ -1,11 +1,12 @@
 #include "Voile.h"
 #include "Orientation.h"
 #include "Communication.h"
+#include "Accelero.h"
 
 int main(void){
-	Voile_Init();
+	/*Voile_Init();
 	Orientation_Init();
-	Communication_Init();
-	
+	Communication_Init();*/
+	Run();
 	while(1);
 }	
